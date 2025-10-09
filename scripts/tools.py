606575777482385c0a555c1c866e5fd93bbaa173
Daniel Biehl <daniel.biehl@imbus.de>
2025-10-10 00:44:12 +0200

    feat: add update version and changelog scripts

---
+++ scripts/tools.py
@@ -0,0 +1,103 @@
+import os
+import re
+from pathlib import Path
+from typing import NamedTuple, Optional
+
+from git.repo import Repo
+from semantic_version import Version
+
+
+class GitDescribeVersion(NamedTuple):
+    version: str
+    commits: Optional[str] = None
+    hash: Optional[str] = None
+
+
+def determine_version_bump(repo_path="."):
+    try:
+        repo = Repo(repo_path)
+        if repo.bare:
+            raise ValueError("Not a valid Git repository.")
+
+        tags = sorted(repo.tags, key=lambda t: t.commit.committed_date, reverse=True)
+        version_tags = [tag for tag in tags if tag.name.startswith("v")]
+        if not version_tags:
+            raise ValueError("No version tags found.")
+
+        last_tag = version_tags[0]
+        commits = repo.iter_commits(f"{last_tag.name}..HEAD")
+
+        ignored_types = ["chore", "style", "refactor", "test"]
+        patch_types = ["fix", "docs", "perf"]
+
+        minor_change = False
+        patch_change = False
+
+        for commit in commits:
+            message = commit.message.strip()
+
+            if any(
+                re.match(rf"^{ignored_type}(\([^\)]+\))?:", message)
+                for ignored_type in ignored_types
+            ):
+                continue
+
+            if "BREAKING CHANGE:" in message or re.match(
+                r"^feat(\([^\)]+\))?!:", message
+            ):
+                return "major"
+
+            if re.match(r"^feat(\([^\)]+\))?:", message):
+                minor_change = True
+
+            if any(
+                re.match(rf"^{patch_types}(\([^\)]+\))?:", message)
+                for ignored_type in ignored_types
+            ):
+                patch_change = True
+
+        if minor_change:
+            return "minor"
+        if patch_change:
+            return "patch"
+        return None
+
+    except Exception as e:
+        raise RuntimeError(f"Error: {e}")
+
+
+def get_current_version_from_git() -> Version:
+    try:
+        repo = Repo(Path.cwd())
+
+        git_version = GitDescribeVersion(
+            *repo.git.describe(
+                "--tag", "--long", "--first-parent", "--match", "v[0-9]*"
+            ).rsplit("-", 2)
+        )
+
+        version = Version(git_version.version[1:])
+        if git_version.commits is not None and git_version.commits != "0":
+            next_version = determine_version_bump()
+            if next_version == "major":
+                version = version.next_major()
+            elif next_version == "minor":
+                version = version.next_minor()
+            elif next_version == "patch":
+                version = version.next_patch()
+            version.prerelease = ("dev", git_version.commits)
+
+        return version
+    except Exception:
+        return Version("0.0.0")
+
+
+def get_version() -> Version:
+    if "CZ_PRE_NEW_VERSION" in os.environ:
+        return Version(os.environ["CZ_PRE_NEW_VERSION"])
+
+    return get_current_version_from_git()
+
+
+if __name__ == "__main__":
+    print(get_version())
---
+++ scripts/update_git_versions.py
@@ -0,0 +1,70 @@
+import contextlib
+import re
+import subprocess
+import sys
+from pathlib import Path
+from typing import Any
+
+if __name__ == "__main__" and not __package__:
+    file = Path(__file__).resolve()
+    parent, top = file.parent, file.parents[1]
+
+    if str(top) not in sys.path:
+        sys.path.append(str(top))
+
+    with contextlib.suppress(ValueError):
+        sys.path.remove(str(parent))
+
+    __package__ = "scripts"
+
+
+from scripts.tools import get_version
+
+
+def replace_in_file(filename: Path, pattern: "re.Pattern[str]", to: str) -> None:
+    text = filename.read_text()
+    new = pattern.sub(to, text)
+    filename.write_text(new)
+
+
+def run(title: str, *args: Any, **kwargs: Any) -> None:
+    try:
+        print(f"running {title}")
+        subprocess.run(*args, **kwargs)
+    except (SystemExit, KeyboardInterrupt):
+        raise
+    except BaseException as e:
+        print(f"{title} failed: {e}", file=sys.stderr)
+
+
+def main() -> None:
+    version = get_version()
+    version_files = list(Path("packages").rglob("__version__.py"))
+
+    for f in [Path("src/PlatynUI/__version__.py"), *version_files]:
+        replace_in_file(
+            f,
+            re.compile(r"""(^_*version_*\s*=\s*['"])([^'"]*)(['"])""", re.MULTILINE),
+            rf"\g<1>{version or ''}\g<3>",
+        )
+
+    pyproject_files = list(Path("packages").rglob("pyproject.toml"))
+    cargo_files = list(Path("packages").rglob("Cargo.toml"))
+
+    for f in [
+        Path("Cargo.toml"),
+        Path("pyproject.toml"),
+        *pyproject_files,
+        *cargo_files,
+    ]:
+        replace_in_file(
+            f,
+            re.compile(
+                r"""("platynui\S*==)([0-9]+\.[0-9]+\.[0-9]+[^'"]*)(")""", re.MULTILINE
+            ),
+            rf"\g<1>{version or ''}\g<3>",
+        )
+
+
+if __name__ == "__main__":
+    main()
