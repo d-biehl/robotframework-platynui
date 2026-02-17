9b0402013e5a03c1125a9ce8a5094b9405175973
Daniel Biehl <dbiehl@live.de>
2026-02-17 17:27:36 +0100

    test: enhance Xephyr configuration for accessibility and language settings

--- scripts/startxsession.sh
+++ scripts/startxsession.sh
@@ -3,7 +3,7 @@
 
 DISPLAY_NUM=100
 
-Xephyr :$DISPLAY_NUM -screen 1920x1080 -nolisten tcp -noreset -dpi 192 &
+Xephyr :$DISPLAY_NUM -ac -screen 1920x1080 -noreset -sw-cursor -dpi 192 &
 XEPHYR_PID=$!
 
 unset DBUS_SESSION_BUS_ADDRESS
@@ -17,11 +17,18 @@ dbus-run-session -- bash -c "
   export XDG_SESSION_TYPE=x11
   export XDG_CURRENT_DESKTOP=xephyr-test
   export NO_AT_BRIDGE=0
+  export ACCESSIBILITY_ENABLED=1
+  export QT_LINUX_ACCESSIBILITY_ALWAYS_ON=1
+  export LANG=de_DE.UTF-8
+  export LC_ALL=de_DE.UTF-8
 
   # optional explizit:
-  /usr/lib/at-spi2-core/at-spi-bus-launcher &
+  /usr/lib/at-spi-bus-launcher &
 
-  exec openbox
+  setxkbmap de -variant e2
+
+  #exec openbox
+  exec startplasma-x11
 "
 
 kill $XEPHYR_PID
